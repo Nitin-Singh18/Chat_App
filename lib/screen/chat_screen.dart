aa5cd4964ce755d0abbb9122b7e312ff93e8c0dd
Nitin Singh <i.nitinsingh18@gmail.com>
2022-07-10 01:39:23 +0530

    Send Image in one to chat

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
 class ChatScreen extends StatelessWidget {
   final Map<String, dynamic> userMap;
@@ -12,11 +17,66 @@ class ChatScreen extends StatelessWidget {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  File? imageFile;
+
+  // function to pick image from gallery
+  Future getImage() async {
+    ImagePicker _picker = ImagePicker();
+    await _picker.pickImage(source: ImageSource.gallery).then((xFile) {
+      if (xFile != null) {
+        imageFile = File(xFile.path);
+        uploadImage();
+      }
+    });
+  }
+
+  //function to upload image to firebasestorage
+  Future uploadImage() async {
+    String fileName = Uuid().v1();
+    int status = 1;
+    await _firestore
+        .collection('chatroom')
+        .doc(chatRoomId)
+        .collection('chats')
+        .doc(fileName)
+        .set({
+      "sendby": _auth.currentUser!.displayName,
+      "message": "",
+      "type": "img",
+      "time": FieldValue.serverTimestamp()
+    });
+
+    var ref =
+        FirebaseStorage.instance.ref().child('images').child("$fileName.jpg");
+    var uploadTask = await ref.putFile(imageFile!).catchError((error) async {
+      await _firestore
+          .collection('chatroom')
+          .doc(chatRoomId)
+          .collection('chats')
+          .doc(fileName)
+          .delete();
+      status = 0;
+    });
+    if (status == 1) {
+      String imageUrl = await uploadTask.ref.getDownloadURL();
+      await _firestore
+          .collection('chatroom')
+          .doc(chatRoomId)
+          .collection('chats')
+          .doc(fileName)
+          .update({
+        "message": imageUrl,
+      });
+      print(imageUrl);
+    }
+  }
+
   void onSendMessage() async {
     if (_message.text.isNotEmpty) {
       Map<String, dynamic> message = {
         "sendby": _auth.currentUser!.displayName,
         "message": _message.text,
+        "type": "text",
         "time": FieldValue.serverTimestamp()
       };
       await _firestore
@@ -40,10 +100,18 @@ class ChatScreen extends StatelessWidget {
             builder: (context, snapshot) {
               if (snapshot.data != null) {
                 return Container(
-                  child: Column(children: [
-                    Text(userMap['name']),
-                    Text(snapshot.data!['status']),
-                  ]),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          userMap['name'],
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        Text(
+                          snapshot.data!['status'],
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ]),
                 );
               } else {
                 return Container();
@@ -65,16 +133,18 @@ class ChatScreen extends StatelessWidget {
                 builder: (BuildContext context,
                     AsyncSnapshot<QuerySnapshot> snapshot) {
                   if (snapshot.data != null) {
-                    return ListView.builder(
-                        itemCount: snapshot.data!.docs.length,
-                        itemBuilder: (context, index) {
-                          Map<String, dynamic>? map = snapshot.data!.docs[index]
-                              .data() as Map<String, dynamic>;
-                          return message(
-                            size,
-                            map,
-                          );
-                        });
+                    if (snapshot.data!.docs.isNotEmpty) {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            Map<String, dynamic>? map =
+                                snapshot.data!.docs[index].data()
+                                    as Map<String, dynamic>;
+                            return message(size, map, context);
+                          });
+                    } else {
+                      return Container();
+                    }
                   } else {
                     return Container();
                   }
@@ -92,6 +162,12 @@ class ChatScreen extends StatelessWidget {
                   child: TextField(
                     controller: _message,
                     decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.photo),
+                        onPressed: () {
+                          getImage();
+                        },
+                      ),
                       hintText: "Type a message",
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
@@ -112,22 +188,71 @@ class ChatScreen extends StatelessWidget {
     );
   }
 
-  Widget message(Size size, Map<String, dynamic> map) {
-    return Container(
-      width: size.width,
-      alignment: map['sendby'] == _auth.currentUser!.displayName
-          ? Alignment.centerRight
-          : Alignment.centerLeft,
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
-        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15), color: Colors.blue),
-        child: Text(
-          map['message'],
-          style: TextStyle(
-              color: Colors.white, fontSize: 16, fontWeight: FontWeight.w500),
-        ),
+  Widget message(Size size, Map<String, dynamic> map, BuildContext context) {
+    return map['type'] == "text"
+        ? Container(
+            width: size.width,
+            alignment: map['sendby'] == _auth.currentUser!.displayName
+                ? Alignment.centerRight
+                : Alignment.centerLeft,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15), color: Colors.blue),
+              child: Text(
+                map['message'],
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500),
+              ),
+            ),
+          )
+        : Container(
+            height: size.height / 2.5,
+            width: size.width,
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+            alignment: map['sendby'] == _auth.currentUser!.displayName
+                ? Alignment.centerRight
+                : Alignment.centerLeft,
+            child: InkWell(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ShowImage(
+                          imageUrl: map['message'],
+                        )));
+              },
+              child: Container(
+                height: size.height / 2.5,
+                width: size.width / 2,
+                decoration: BoxDecoration(border: Border.all()),
+                alignment: map['message'] != "" ? null : Alignment.center,
+                child: map['message'] != ""
+                    ? Image.network(
+                        map['message'],
+                        fit: BoxFit.cover,
+                      )
+                    : CircularProgressIndicator(),
+              ),
+            ),
+          );
+  }
+}
+
+class ShowImage extends StatelessWidget {
+  final String imageUrl;
+  const ShowImage({Key? key, required this.imageUrl}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        color: Colors.black,
+        child: Image.network(imageUrl),
       ),
     );
   }
